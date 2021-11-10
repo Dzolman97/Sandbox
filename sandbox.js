e7f76b29a2e3eb58d7dd60cba11d0452adc69ca2
Daniel Zolman <zolman.daniel@gmail.com>
2021-11-09 22:35:05 -0700

    turned it into a joke question and answer. How do I combine multiple answers into one if?

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,29 @@
-// console.log("Hello World!")
+const readline = require("readline");
 
-let message = "Hello again, world!"
-console.log(message)
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+reader.question("Hey! Do you want to hear a joke?", function(answer){
+    if (answer === "Yes") {
+        console.log("Awesome!");
+    }
+
+    if (answer === "yes") {
+        console.log("Awesome!");
+    }
+
+    if (answer === "y") {
+        console.log("Awesome!");
+    }
+
+    if (answer === "No") {
+        console.log("Well, too bad. You'll hear it anyway... Knock, Knock!")
+    }
+
+    if (answer === "no") {
+        console.log("Well, too bad. You'll hear it anyway... Knock, Knock!")
+    }
+
+})
